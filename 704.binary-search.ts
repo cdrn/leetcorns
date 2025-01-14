20777f0e498afcf0d917c06b92c28885599cbe39
cdrn <chrisdoran@protonmail.com>
2025-01-14 17:15:47 +1100

    feat: binary search also

---
+++ 704.binary-search.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=704 lang=typescript
+ *
+ * [704] Binary Search
+ */
+
+// @lc code=start
+function search(nums: number[], target: number): number {
+  let left = 0;
+  let right = nums.length - 1;
+
+  // While we're not on top of eachother
+  while (left <= right) {
+    let mid = Math.floor((left + right) / 2);
+    if (nums[mid] === target) {
+      return mid;
+    }
+    if (nums[mid] < target) {
+      left = mid + 1;
+    } else {
+      right = mid - 1;
+    }
+  }
+
+  return -1;
+}
+// @lc code=end
