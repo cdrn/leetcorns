fa1e7b5ea1f6bb7ef4a0a95678747adffa009e8b
cdrn <chrisdoran@protonmail.com>
2025-01-30 23:16:04 +1100

    feat: merge sorted

--- 88.merge-sorted-array.ts
+++ 88.merge-sorted-array.ts
@@ -9,7 +9,19 @@
  Do not return anything, modify nums1 in-place instead.
  */
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    
-};
-// @lc code=end
+  let i = m + n - 1; // last position in nums1
+  let p1 = m - 1; // last element in nums1
+  let p2 = n - 1; // last element in nums2
 
+  while (p2 >= 0) {
+    if (p1 >= 0 && nums1[p1] > nums2[p2]) {
+      nums1[i] = nums1[p1]; // move nums1[p1]
+      p1--;
+    } else {
+      nums1[i] = nums2[p2]; // move nums2[p2]
+      p2--;
+    }
+    i--; // move to next position
+  }
+}
+// @lc code=end
